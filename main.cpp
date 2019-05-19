91f287e81d76026ffb86b685df046d46ef583931
alqaz y <alqazy@alqazdeMBP.lan>
2019-05-19 18:25:26 +0800

    add logic or test

--- main.cpp
+++ main.cpp
@@ -19,8 +19,18 @@ void test_if_def()
   #endif
 }
 
+void test_def_logic_or()
+{
+  #if defined(IF_FIRST) || defined(IF_SECOND)
+  cout << "has defined IF_FIRST or IF_SECOND" << endl;
+  #else
+  cout << "neither IF_FIRST nor IF_SECOND defined " << endl;
+  #endif
+}
+
 int main()
 {
    func();
    test_if_def();
+   test_def_logic_or();
 }
